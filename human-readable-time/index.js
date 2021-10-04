c17cd79b841b1d3fee3c9f34809c21999504b427
Benjamin Buccianti <benjamin@buccianti.dev>
2021-10-04 15:04:06 -0300

    human readable time

---
+++ human-readable-time/index.js
@@ -0,0 +1,7 @@
+function humanReadable(s) {
+  return [s / 3600, (s % 3600) / 60, (s % 3600) % 60]
+    .map((v) => String(v >> 0).padStart(2, "0"))
+    .join(":");
+}
+
+module.exports = humanReadable;
