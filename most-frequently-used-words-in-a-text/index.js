b01d1604a9eedbfdd4be0d0cf2159fe7ded7690a
Benjamin Buccianti <benjamin@buccianti.dev>
2021-10-04 13:37:44 -0300

    Most frequently words in a text

---
+++ most-frequently-used-words-in-a-text/index.js
@@ -0,0 +1,21 @@
+function topThreeWords(text) {
+  return text
+    .replace(/[^\w' ]| ' /gi, "")
+    .toLowerCase()
+    .split(" ")
+    .filter((b) => b)
+    .reduce(
+      ([acc, _], next) => [
+        ((acc[next] = (acc[next] ?? 0) + 1), acc),
+        Object.entries(acc),
+      ],
+      [{}, []]
+    )
+    .slice(-1)
+    .pop()
+    .sort((a, b) => b[1] - a[1])
+    .map((v) => v[0])
+    .slice(0, 3);
+}
+
+module.exports = topThreeWords;
---
+++ most-frequently-used-words-in-a-text/index.test.js
@@ -0,0 +1,40 @@
+const chai = require("chai");
+const topThreeWords = require("./index");
+
+describe("Tests", () => {
+  it("test", () => {
+    chai.assert.deepEqual(topThreeWords("a a a  b  c c  d d d d  e e e e e"), [
+      "e",
+      "d",
+      "a",
+    ]);
+
+    chai.assert.deepEqual(topThreeWords("a a c b b"), ["a", "b", "c"]);
+
+    chai.assert.deepEqual(
+      topThreeWords("e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e"),
+      ["e", "ddd", "aa"]
+    );
+
+    chai.assert.deepEqual(topThreeWords("  //wont won't won't "), [
+      "won't",
+      "wont",
+    ]);
+
+    chai.assert.deepEqual(topThreeWords("  , e   .. "), ["e"]);
+
+    chai.assert.deepEqual(topThreeWords("  ...  "), []);
+
+    chai.assert.deepEqual(topThreeWords("  '  "), []);
+
+    chai.assert.deepEqual(
+      topThreeWords(`In a village of La Mancha, the name of which I have no desire to call to
+mind, there lived not long since one of those gentlemen that keep a lance
+in the lance-rack, an old buckler, a lean hack, and a greyhound for
+coursing. An olla of rather more beef than mutton, a salad on most
+nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra
+on Sundays, made away with three-quarters of his income.`),
+      ["a", "of", "on"]
+    );
+  });
+});
